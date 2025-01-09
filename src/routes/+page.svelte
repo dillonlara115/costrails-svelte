7ced0fc5a4a77615883be7b6fdc96542b156edda
Dillon Lara <dillonlara115@gmail.com>
2025-01-09 13:27:07 -0700

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	
+</script>
+
+<div class="mx-auto w-full pt-5">
+	<article class="prose mx-auto max-w-4xl lg:prose-xl">
+		<h1>Colorado Springs MTB Trail Conditions</h1>
+		<p>
+			Half of my web projects always start the same way so I figured I could save myself some time
+			and create a template.
+		</p>
+		<!-- BEGIN MTB Project -->
+<iframe style="width:100%; max-width:1200px; height:700px;" frameborder="0" scrolling="no" src="https://mtbproject.com/widget/conditions?v=3&x=-11655352&y=4699274&z=5&height=400"></iframe>
+<!-- END MTB Project -->
+	</article>
+</div>
